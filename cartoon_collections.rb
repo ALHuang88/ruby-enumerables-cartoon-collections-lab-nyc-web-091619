f392376f18df74b0ad97d0ff654f7079df97202b
Allen Huang <alhuang1288@gmail.com>
2019-08-28 17:08:24 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,9 +15,8 @@ def summon_captain_planet(array)
      word.capitalize + "!"
    end 
  end 
+summon_captain_planet(planeteer_calls)
 
- summon_captain_planet(planeteer_calls)
-end
 
 def long_planeteer_calls# code an argument here
   # Your code here

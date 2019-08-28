00cf62b1eaef31cce72a2367c79ff6c70078d18d
Allen Huang <alhuang1288@gmail.com>
2019-08-28 17:09:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,9 +17,10 @@ def summon_captain_planet(array)
  end 
 summon_captain_planet(planeteer_calls)
 
-
-def long_planeteer_calls# code an argument here
-  # Your code here
+short_words = ["puff", "go", "two"]
+ assorted_words = ["two", "go", "industrious", "bop"]
+def long_planeteer_calls(array)
+  
 end
 
 def find_the_cheese# code an argument here
